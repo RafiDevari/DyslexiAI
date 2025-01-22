b1a8a6e226806b70cad2b2b0521324e597032dc1
RafiDevari <rafidevari2004@gmail.com>
2025-01-22 16:49:18 +0700

    GPT go 8.5 (nambah jadi karousel biar enak)

--- lib/susunkata/levelSelector.dart
+++ lib/susunkata/levelSelector.dart
@@ -2,6 +2,12 @@ import 'package:dyslexiai/susunkata/kataSelector.dart';
 import 'package:flutter/material.dart';
 
 class Susunkatalevelselector extends StatelessWidget {
+  final List<Map<String, String>> categories = [
+    {"title": "Hewan Darat  (Swipe ke kanan )", "type": "Darat"},
+    {"title": "Hewan Laut", "type": "Laut"},
+    {"title": "Hewan Udara", "type": "Udara"},
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,47 +15,39 @@ class Susunkatalevelselector extends StatelessWidget {
         title: Text('Susunkatalobi'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => kataSelector(type: "Darat")),
-                );
-              },
-              child: Text('Hewan Darat'),
-            ),
-
-            SizedBox(height: 20),
-
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => kataSelector(type: "Laut",)),
-                );
-              },
-              child: Text('Hewan Laut'),
-            ),
-
-            SizedBox(height: 20),
-
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => kataSelector(type: "Udara",)),
-                );
-              },
-              child: Text('Hewan Udara'),
-            ),
-
-            SizedBox(height: 20),
-
-
-          ],
+        child: Container(
+          height: 200,
+          child: PageView.builder(
+            itemCount: categories.length,
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (context, index) {
+              return GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => kataSelector(type: categories[index]["type"]!),
+                    ),
+                  );
+                },
+                child: Card(
+                  elevation: 5,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    alignment: Alignment.center,
+                    child: Text(
+                      categories[index]["title"]!,
+                      style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
         ),
       ),
     );
